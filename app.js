64c63d74feb1b0a9f72e692f64cd3431235940cb
holyhansss <63653067+holyhansss@users.noreply.github.com>
2021-07-17 20:35:04 +0900

    Add files via upload
    added:
       color change
       canvas background change
       save image(as png)

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext('2d');
-const color = document.getElementById("controls_colors");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 canvas.width = 700;
 canvas.height = 700;
 
+ctx.fillStyle="white";
+ctx.fillRect(0,0,canvas.width, canvas.height);
 ctx.strokeStyle="#2c2c2c";
+ctx.fillStyle="2c2c2c";
 ctx.lineWidth = 2.5;
 
+
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -30,20 +38,63 @@ function onMouseMove(event){
     }
 }
 
-function onMouseDown(event){
-    painting = true;
-    
-}
-function onClickChangeColor(event){
-    console.log(event);
+function handleColorClick(event){
+    const bgcolor = event.target.style.backgroundColor;
+    ctx.strokeStyle = bgcolor;
+    ctx.fillStyle = bgcolor;
 }
 
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(event) {
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+        ctx.fillStuye = ctx.strokeStyle;
+    }
+
+}
+function handleCanvasClick(){
+    if(filling){
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    }
+}
+
+function handleCM(event) {
+    event.preventDefault();
+}
+function handleSaveClick(event){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "paintJS";
+    link.click();
+}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
-    color.addEventListener("click", onClickChangeColor);
+    canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+}
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColorClick));
+if(range){
+    range.addEventListener("input", handleRangeChange);
 }
 
+if(mode) {
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick)
+}
